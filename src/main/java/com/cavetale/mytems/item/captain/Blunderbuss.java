db1832470a59172e4fad84eb19c9fbe6fc2aaeef
StarTux <ths.loy@googlemail.com>
2021-07-24 21:43:56 +0200

    Single use option

--- src/main/java/com/cavetale/mytems/item/captain/Blunderbuss.java
+++ src/main/java/com/cavetale/mytems/item/captain/Blunderbuss.java
@@ -5,6 +5,7 @@ import com.cavetale.mytems.Mytems;
 import com.cavetale.mytems.MytemsPlugin;
 import com.cavetale.mytems.session.Session;
 import com.cavetale.mytems.util.Text;
+import com.cavetale.worldmarker.util.Tags;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import net.kyori.adventure.text.Component;
@@ -15,6 +16,7 @@ import org.bukkit.FluidCollisionMode;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
 import org.bukkit.SoundCategory;
@@ -37,6 +39,7 @@ public final class Blunderbuss implements Mytem {
         + "Use the mighty blunderbuss t' hit yer enemies from afar.";
     private final TextColor titleColor = TextColor.color(0xddd417);
     private final TextColor loreColor = TextColor.color(0x760a0a);
+    @Getter private static NamespacedKey singleUseKey;
 
     @Override
     public void enable() {
@@ -50,6 +53,7 @@ public final class Blunderbuss implements Mytem {
         meta.addItemFlags(ItemFlag.values());
         key.markItemMeta(meta);
         prototype.setItemMeta(meta);
+        singleUseKey = new NamespacedKey(MytemsPlugin.getInstance(), "single_use");
     }
 
     @Override
@@ -65,7 +69,10 @@ public final class Blunderbuss implements Mytem {
     @Override
     public void onPlayerRightClick(PlayerInteractEvent event, Player player, ItemStack item) {
         event.setCancelled(true);
-        pullTheTrigger(player);
+        boolean result = pullTheTrigger(player);
+        if (result && Tags.getInt(item.getItemMeta().getPersistentDataContainer(), singleUseKey) != null) {
+            item.subtract(1);
+        }
     }
 
     public boolean pullTheTrigger(Player player) {
