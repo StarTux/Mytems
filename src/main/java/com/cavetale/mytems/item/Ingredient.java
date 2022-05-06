3d2be1d4ecbfda91c864ffe203883b49dd2571b0
StarTux <ths.loy@googlemail.com>
2022-05-06 16:56:03 +0200

    Strip the display name

--- src/main/java/com/cavetale/mytems/item/Ingredient.java
+++ src/main/java/com/cavetale/mytems/item/Ingredient.java
@@ -2,39 +2,36 @@ package com.cavetale.mytems.item;
 
 import com.cavetale.mytems.Mytem;
 import com.cavetale.mytems.Mytems;
-import com.cavetale.mytems.util.Text;
+import com.cavetale.mytems.util.Items;
+import java.util.List;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.TextColor;
-import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
+import static com.cavetale.mytems.util.Text.toCamelCase;
+import static net.kyori.adventure.text.Component.text;
 
 @RequiredArgsConstructor @Getter
 public final class Ingredient implements Mytem {
-    private static TextColor color = TextColor.color(0xafa259);
+    private static final TextColor COLOR = TextColor.color(0xafa259);
     private final Mytems key;
     private ItemStack prototype;
     private Component displayName;
 
     @Override
     public void enable() {
-        displayName = Component.text()
-            .append(key.component)
-            .append(Component.text(Text.toCamelCase(key, " "), color))
-            .decoration(TextDecoration.ITALIC, false)
-            .build();
+        displayName = text(toCamelCase(key, " "), COLOR);
         prototype = new ItemStack(key.material);
-        ItemMeta meta = prototype.getItemMeta();
-        meta.displayName(displayName);
-        meta.addItemFlags(ItemFlag.values());
-        key.markItemMeta(meta);
-        prototype.setItemMeta(meta);
+        prototype.editMeta(meta -> {
+                Items.text(meta, List.of(displayName));
+                meta.addItemFlags(ItemFlag.values());
+                key.markItemMeta(meta);
+            });
     }
 
     @Override
