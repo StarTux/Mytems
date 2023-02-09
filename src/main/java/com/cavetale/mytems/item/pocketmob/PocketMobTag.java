091132a4c79fc27c8e170b92b23893853f8859d7
StarTux <ths.loy@googlemail.com>
2023-02-09 15:36:24 +0100

    Remove legacy mob (de)serialization

--- src/main/java/com/cavetale/mytems/item/pocketmob/PocketMobTag.java
+++ src/main/java/com/cavetale/mytems/item/pocketmob/PocketMobTag.java
@@ -5,14 +5,11 @@ import com.cavetale.mytems.Mytems;
 import com.cavetale.mytems.util.BlockColor;
 import com.cavetale.mytems.util.Entities;
 import com.cavetale.mytems.util.Items;
-import com.cavetale.mytems.util.Json;
 import com.cavetale.mytems.util.Text;
 import com.cavetale.worldmarker.util.Tags;
 import java.util.ArrayList;
 import java.util.Base64;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -65,16 +62,14 @@ import static net.kyori.adventure.text.format.TextColor.color;
  */
 @Data @EqualsAndHashCode(callSuper = true)
 public final class PocketMobTag extends MytemTag {
-    public static final NamespacedKey KEY_MOB_TAG = NamespacedKey.fromString("pocketmob:mob_tag");
     public static final NamespacedKey KEY_MOB = NamespacedKey.fromString("pocketmob:mob");
     public static final TextColor COLOR_FG = color(0xffa600); // gold
     public static final TextColor COLOR_BG = color(0x3f274d); // pale purle
-    protected String mobTag; // Legacy, Dirty
     protected String mob; // UnsafeValues#serializeEntity, Base64
 
     @Override
     public boolean isEmpty() {
-        return mobTag == null && mob == null && super.isEmpty();
+        return mob == null && super.isEmpty();
     }
 
     public void load(ItemStack itemStack, PocketMob pocketMob) {
@@ -82,7 +77,6 @@ public final class PocketMobTag extends MytemTag {
         if (!itemStack.hasItemMeta()) return;
         ItemMeta meta = itemStack.getItemMeta();
         PersistentDataContainer tag = meta.getPersistentDataContainer();
-        mobTag = Tags.getString(tag, KEY_MOB_TAG);
         mob = Tags.getString(tag, KEY_MOB);
     }
 
@@ -111,156 +105,150 @@ public final class PocketMobTag extends MytemTag {
      */
     public void store(ItemStack itemStack, PocketMob pocketMob) {
         super.store(itemStack);
-        ItemMeta meta = itemStack.getItemMeta();
+        itemStack.editMeta(meta -> store(meta, pocketMob));
+    }
+
+    public void store(ItemMeta meta, PocketMob pocketMob) {
         PersistentDataContainer tag = meta.getPersistentDataContainer();
-        if (mobTag != null) {
-            Tags.set(tag, KEY_MOB_TAG, mobTag);
+        if (mob == null) {
+            Items.text(meta, List.of(pocketMob.getDisplayName()));
+            tag.remove(KEY_MOB);
+            return;
         }
         List<Component> text = new ArrayList<>();
         text.add(null);
-        if (mob != null) {
-            Tags.set(tag, KEY_MOB, mob);
-            Entity entity = deserializeMob(Bukkit.getWorlds().get(0));
-            if (entity != null) {
-                Component customName = entity.customName();
-                text.set(0, customName);
+        Tags.set(tag, KEY_MOB, mob);
+        Entity entity = deserializeMob(Bukkit.getWorlds().get(0));
+        if (entity != null) {
+            Component customName = entity.customName();
+            text.set(0, customName);
+        }
+        if (entity instanceof Mob mobEntity) {
+            Component customName = mobEntity.customName();
+            if (customName != null && !empty().equals(customName)) {
+                text.add(prop("Name", customName));
             }
-            if (entity instanceof Mob mobEntity) {
-                Component customName = mobEntity.customName();
-                if (customName != null && !empty().equals(customName)) {
-                    text.add(prop("Name", customName));
-                }
-                String health = "" + (int) Math.ceil(mobEntity.getHealth());
-                String maxHealth = "" + (int) Math.ceil(mobEntity.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue());
-                text.add(prop("Health", textOfChildren(Mytems.HEART, text(health, RED), text("/", COLOR_BG), text(maxHealth, RED))));
-                if ((!(mobEntity instanceof Animals) && mobEntity.getRemoveWhenFarAway())
-                    || !mobEntity.isPersistent() || Entities.isTransient(mobEntity)) {
-                    text.add(prop("Warning", text("May Despawn!", color(0xFF0000))));
-                }
-            }
-            List<String> nameComponents = new ArrayList<>();
-            String finalNameComponent = Text.toCamelCase(entity.getType());
-            if (entity instanceof Ageable ageable) {
-                if (!ageable.isAdult()) nameComponents.add("Baby");
-            }
-            if (entity instanceof Creeper creeper) {
-                if (creeper.isPowered()) {
-                    nameComponents.add(0, "Powered");
-                }
-            } else if (entity instanceof Wither wither) {
-                if (wither.isCharged()) {
-                    nameComponents.add(0, "Charged");
-                }
-            } else if (entity instanceof AbstractHorse ahorse) {
-                if (ahorse instanceof Horse horse) {
-                    nameComponents.add(Text.toCamelCase(horse.getColor()));
-                    nameComponents.add(Text.toCamelCase(horse.getStyle()));
-                }
-                if (ahorse instanceof Llama llama) {
-                    nameComponents.add(Text.toCamelCase(llama.getColor()));
-                    text.add(prop("Strength", "" + llama.getStrength()));
-                }
-                text.add(prop("Jump", "" + (int) Math.round(100.0 * ahorse.getJumpStrength())));
-                text.add(prop("Speed", "" + (int) Math.round(100.0 * ahorse.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).getValue())));
-            } else if (entity instanceof Colorable colorable) { // Sheep, Shulker
-                DyeColor dyeColor = colorable.getColor();
-                if (dyeColor != null) {
-                    BlockColor blockColor = BlockColor.of(dyeColor);
-                    text.add(prop("Color", blockColor));
-                    nameComponents.add(0, blockColor.niceName);
-                }
-            } else if (entity instanceof Cat cat) {
-                nameComponents.add(Text.toCamelCase(cat.getCatType()));
-                text.add(prop("Collar", BlockColor.of(cat.getCollarColor())));
-            } else if (entity instanceof Parrot parrot) {
-                nameComponents.add(Text.toCamelCase(parrot.getVariant()));
-            } else if (entity instanceof Frog frog) {
-                nameComponents.add(Text.toCamelCase(frog.getVariant()));
-            } else if (entity instanceof Wolf wolf) {
-                if (wolf.isAngry()) {
-                    nameComponents.add(0, "Angry");
-                }
-                text.add(prop("Collar", BlockColor.of(wolf.getCollarColor())));
-            } else if (entity instanceof Bee bee) {
-                if (bee.getAnger() > 0) {
-                    nameComponents.add(0, "Angry");
-                }
-                text.add(prop("Nectar", bee.hasNectar()));
-                text.add(prop("Sting", !bee.hasStung()));
-            } else if (entity instanceof PufferFish pufferfish) {
-                if (pufferfish.getPuffState() > 0) {
-                    nameComponents.add(0, "Inflated");
-                }
-            } else if (entity instanceof Fox fox) {
-                nameComponents.add(Text.toCamelCase(fox.getFoxType()));
-                if (fox.isSleeping()) {
-                    nameComponents.add(0, "Sleeping");
-                }
-            } else if (entity instanceof Rabbit rabbit) {
-                nameComponents.add(Text.toCamelCase(rabbit.getRabbitType()));
-                if (rabbit.getRabbitType() == Rabbit.Type.THE_KILLER_BUNNY) {
-                    finalNameComponent = null;
-                }
-            } else if (entity instanceof Steerable steerable) { // Pig, Strider
-                if (steerable instanceof Strider strider) {
-                    if (strider.isShivering()) {
-                        nameComponents.add(0, "Shivering");
-                    }
-                }
-                if (steerable.hasSaddle()) {
-                    nameComponents.add(0, "Saddled");
-                }
-            } else if (entity instanceof TropicalFish tropicalFish) {
-                nameComponents.add(Text.toCamelCase(tropicalFish.getBodyColor()));
-                finalNameComponent = Text.toCamelCase(tropicalFish.getPattern());
-            } else if (entity instanceof Villager villager) {
-                if (villager.getProfession() != Villager.Profession.NONE) {
-                    finalNameComponent = Text.toCamelCase(villager.getProfession());
-                }
-                nameComponents.add(Text.toCamelCase(villager.getVillagerType()));
-                text.add(prop("Level", "" + villager.getVillagerLevel()));
-            } else if (entity instanceof ZombieVillager zombieVillager) {
-                finalNameComponent = Text.toCamelCase(zombieVillager.getVillagerProfession());
-                nameComponents.add(Text.toCamelCase(zombieVillager.getVillagerType()));
-            } else if (entity instanceof Slime slime) {
-                text.add(prop("Size", "" + slime.getSize()));
-            } else if (entity instanceof Raider raider) {
-                if (raider.isPatrolLeader()) {
-                    nameComponents.add("Chief");
-                }
-                if (raider instanceof Spellcaster spellcaster) {
-                    if (spellcaster.getSpell() != Spellcaster.Spell.NONE) {
-                        text.add(prop("Spell", Text.toCamelCase(spellcaster.getSpell())));
-                    }
-                }
-            }
-            if (entity instanceof Tameable tameable) {
-                if (tameable.isTamed()) {
-                    nameComponents.add(0, "Tamed");
-                }
-            }
-            if (entity.isGlowing()) {
-                nameComponents.add(0, "Glowing");
-            }
-            if (finalNameComponent != null) {
-                nameComponents.add(finalNameComponent);
-            }
-            if (text.get(0) == null) {
-                text.set(0, text(String.join(" ", nameComponents), COLOR_FG));
-            } else {
-                text.add(1, text(String.join(" ", nameComponents), COLOR_BG));
-            }
-        } else {
-            Map<String, Object> map = parseMobTag();
-            Object o;
-            o = map.get("Health");
-            if (o instanceof Number) {
-                text.add(textOfChildren(text("Health ", COLOR_BG),
-                                        text(((Number) o).intValue(), COLOR_FG)));
+            String health = "" + (int) Math.ceil(mobEntity.getHealth());
+            String maxHealth = "" + (int) Math.ceil(mobEntity.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue());
+            text.add(prop("Health", textOfChildren(Mytems.HEART, text(health, RED), text("/", COLOR_BG), text(maxHealth, RED))));
+            if ((!(mobEntity instanceof Animals) && mobEntity.getRemoveWhenFarAway())
+                || !mobEntity.isPersistent() || Entities.isTransient(mobEntity)) {
+                text.add(prop("Warning", text("May Despawn!", color(0xFF0000))));
             }
         }
+        List<String> nameComponents = new ArrayList<>();
+        String finalNameComponent = Text.toCamelCase(entity.getType());
+        if (entity instanceof Ageable ageable) {
+            if (!ageable.isAdult()) nameComponents.add("Baby");
+        }
+        if (entity instanceof Creeper creeper) {
+            if (creeper.isPowered()) {
+                nameComponents.add(0, "Powered");
+            }
+        } else if (entity instanceof Wither wither) {
+            if (wither.isCharged()) {
+                nameComponents.add(0, "Charged");
+            }
+        } else if (entity instanceof AbstractHorse ahorse) {
+            if (ahorse instanceof Horse horse) {
+                nameComponents.add(Text.toCamelCase(horse.getColor()));
+                nameComponents.add(Text.toCamelCase(horse.getStyle()));
+            }
+            if (ahorse instanceof Llama llama) {
+                nameComponents.add(Text.toCamelCase(llama.getColor()));
+                text.add(prop("Strength", "" + llama.getStrength()));
+            }
+            text.add(prop("Jump", "" + (int) Math.round(100.0 * ahorse.getJumpStrength())));
+            text.add(prop("Speed", "" + (int) Math.round(100.0 * ahorse.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).getValue())));
+        } else if (entity instanceof Colorable colorable) { // Sheep, Shulker
+            DyeColor dyeColor = colorable.getColor();
+            if (dyeColor != null) {
+                BlockColor blockColor = BlockColor.of(dyeColor);
+                text.add(prop("Color", blockColor));
+                nameComponents.add(0, blockColor.niceName);
+            }
+        } else if (entity instanceof Cat cat) {
+            nameComponents.add(Text.toCamelCase(cat.getCatType()));
+            text.add(prop("Collar", BlockColor.of(cat.getCollarColor())));
+        } else if (entity instanceof Parrot parrot) {
+            nameComponents.add(Text.toCamelCase(parrot.getVariant()));
+        } else if (entity instanceof Frog frog) {
+            nameComponents.add(Text.toCamelCase(frog.getVariant()));
+        } else if (entity instanceof Wolf wolf) {
+            if (wolf.isAngry()) {
+                nameComponents.add(0, "Angry");
+            }
+            text.add(prop("Collar", BlockColor.of(wolf.getCollarColor())));
+        } else if (entity instanceof Bee bee) {
+            if (bee.getAnger() > 0) {
+                nameComponents.add(0, "Angry");
+            }
+            text.add(prop("Nectar", bee.hasNectar()));
+            text.add(prop("Sting", !bee.hasStung()));
+        } else if (entity instanceof PufferFish pufferfish) {
+            if (pufferfish.getPuffState() > 0) {
+                nameComponents.add(0, "Inflated");
+            }
+        } else if (entity instanceof Fox fox) {
+            nameComponents.add(Text.toCamelCase(fox.getFoxType()));
+            if (fox.isSleeping()) {
+                nameComponents.add(0, "Sleeping");
+            }
+        } else if (entity instanceof Rabbit rabbit) {
+            nameComponents.add(Text.toCamelCase(rabbit.getRabbitType()));
+            if (rabbit.getRabbitType() == Rabbit.Type.THE_KILLER_BUNNY) {
+                finalNameComponent = null;
+            }
+        } else if (entity instanceof Steerable steerable) { // Pig, Strider
+            if (steerable instanceof Strider strider) {
+                if (strider.isShivering()) {
+                    nameComponents.add(0, "Shivering");
+                }
+            }
+            if (steerable.hasSaddle()) {
+                nameComponents.add(0, "Saddled");
+            }
+        } else if (entity instanceof TropicalFish tropicalFish) {
+            nameComponents.add(Text.toCamelCase(tropicalFish.getBodyColor()));
+            finalNameComponent = Text.toCamelCase(tropicalFish.getPattern());
+        } else if (entity instanceof Villager villager) {
+            if (villager.getProfession() != Villager.Profession.NONE) {
+                finalNameComponent = Text.toCamelCase(villager.getProfession());
+            }
+            nameComponents.add(Text.toCamelCase(villager.getVillagerType()));
+            text.add(prop("Level", "" + villager.getVillagerLevel()));
+        } else if (entity instanceof ZombieVillager zombieVillager) {
+            finalNameComponent = Text.toCamelCase(zombieVillager.getVillagerProfession());
+            nameComponents.add(Text.toCamelCase(zombieVillager.getVillagerType()));
+        } else if (entity instanceof Slime slime) {
+            text.add(prop("Size", "" + slime.getSize()));
+        } else if (entity instanceof Raider raider) {
+            if (raider.isPatrolLeader()) {
+                nameComponents.add("Chief");
+            }
+            if (raider instanceof Spellcaster spellcaster) {
+                if (spellcaster.getSpell() != Spellcaster.Spell.NONE) {
+                    text.add(prop("Spell", Text.toCamelCase(spellcaster.getSpell())));
+                }
+            }
+        }
+        if (entity instanceof Tameable tameable) {
+            if (tameable.isTamed()) {
+                nameComponents.add(0, "Tamed");
+            }
+        }
+        if (entity.isGlowing()) {
+            nameComponents.add(0, "Glowing");
+        }
+        if (finalNameComponent != null) {
+            nameComponents.add(finalNameComponent);
+        }
+        if (text.get(0) == null) {
+            text.set(0, text(String.join(" ", nameComponents), COLOR_FG));
+        } else {
+            text.add(1, text(String.join(" ", nameComponents), COLOR_BG));
+        }
         Items.text(meta, text);
-        itemStack.setItemMeta(meta);
     }
 
     @SuppressWarnings("deprecation")
@@ -276,19 +264,6 @@ public final class PocketMobTag extends MytemTag {
         mob = Base64.getEncoder().encodeToString(byteArray);
     }
 
-    @Deprecated
-    public Map<String, Object> parseMobTag() {
-        if (mobTag == null) return new HashMap<>();
-        try {
-            @SuppressWarnings("unchecked")
-                Map<String, Object> result = (Map<String, Object>) Json.deserialize(mobTag, Map.class);
-            return result;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return new HashMap<>();
-        }
-    }
-
     @Override
     public boolean isDismissable() {
         return false;
@@ -298,7 +273,6 @@ public final class PocketMobTag extends MytemTag {
     public boolean isSimilar(MytemTag other) {
         return super.isSimilar(other)
             && other instanceof PocketMobTag that
-            && Objects.equals(this.mobTag, that.mobTag)
             && Objects.equals(this.mob, that.mob);
     }
 }
