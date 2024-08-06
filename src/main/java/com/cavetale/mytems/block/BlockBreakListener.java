f486217a70eaf1b23eea670b250efc99e0ed72eb
StarTux <ths.loy@gmail.com>
2024-08-06 14:35:41 +0200

    Break blocks without physics

--- src/main/java/com/cavetale/mytems/block/BlockBreakListener.java
+++ src/main/java/com/cavetale/mytems/block/BlockBreakListener.java
@@ -2,10 +2,13 @@ package com.cavetale.mytems.block;
 
 import com.cavetale.core.event.block.PlayerBreakBlockEvent;
 import com.cavetale.mytems.MytemsPlugin;
+import java.util.List;
 import java.util.function.Consumer;
 import lombok.RequiredArgsConstructor;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -45,6 +48,31 @@ public final class BlockBreakListener implements Listener {
         return breakBlock(player, tool, block, itemSpawnEvent -> {
                 itemSpawnEvent.getEntity().teleport(player.getLocation());
                 itemSpawnEvent.getEntity().setPickupDelay(0);
+            });
+    }
+
+    public boolean breakBlockNoPhysics(Player player, ItemStack tool, Block block, final Consumer<List<ItemStack>> dropCallback) {
+        if (!new PlayerBreakBlockEvent(player, block, tool).callEvent()) return false;
+        final List<ItemStack> drops;
+        if (player != null && tool != null) {
+            drops = List.copyOf(block.getDrops(tool, player));
+        } else if (tool != null) {
+            drops = List.copyOf(block.getDrops(tool));
+        } else {
+            drops = List.copyOf(block.getDrops());
+        }
+        final boolean applyPhysics = false;
+        block.setType(Material.AIR, applyPhysics);
+        dropCallback.accept(drops);
+        return true;
+    }
+
+    public boolean breakBlockNoPhysicsAndPickup(Player player, ItemStack tool, Block block) {
+        return breakBlockNoPhysics(player, tool, block, drops -> {
+                for (ItemStack drop : drops) {
+                    final Item item = player.getWorld().dropItem(player.getLocation(), drop);
+                    item.setPickupDelay(0);
+                }
             });
     }
 
