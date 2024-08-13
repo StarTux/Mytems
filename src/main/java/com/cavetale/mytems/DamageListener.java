bb01140844738eac3919248f5d73041851f0a3a6
StarTux <ths.loy@gmail.com>
2024-08-13 19:13:45 +0200

    Add missing import

--- src/main/java/com/cavetale/mytems/DamageListener.java
+++ src/main/java/com/cavetale/mytems/DamageListener.java
@@ -14,6 +14,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 
