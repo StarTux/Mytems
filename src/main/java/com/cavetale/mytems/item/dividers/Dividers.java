fc3f3b1a5de7490eae81be8f1bdaf98fe4aaf25c
StarTux <ths.loy@gmail.com>
2024-09-09 19:28:57 +0200

    Dividers use Cuboid Outlines

--- src/main/java/com/cavetale/mytems/item/dividers/Dividers.java
+++ src/main/java/com/cavetale/mytems/item/dividers/Dividers.java
@@ -1,30 +1,38 @@
 package com.cavetale.mytems.item.dividers;
 
 import com.cavetale.core.event.block.PlayerBlockAbilityQuery;
+import com.cavetale.core.struct.Cuboid;
 import com.cavetale.core.struct.Vec2i;
 import com.cavetale.core.struct.Vec3i;
 import com.cavetale.mytems.Mytem;
 import com.cavetale.mytems.Mytems;
-import com.cavetale.mytems.MytemsPlugin;
+import com.cavetale.mytems.item.axis.CuboidOutline;
+import com.cavetale.mytems.session.Session;
+import com.cavetale.mytems.util.Attr;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import lombok.Getter;
 import net.kyori.adventure.text.Component;
-import org.bukkit.Bukkit;
+import org.bukkit.Axis;
+import org.bukkit.Color;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.SoundCategory;
 import org.bukkit.World;
+import org.bukkit.attribute.Attribute;
 import org.bukkit.block.Block;
-import org.bukkit.block.data.BlockData;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlotGroup;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import static com.cavetale.core.font.Unicode.tiny;
-import static com.cavetale.mytems.MytemsPlugin.sessionOf;
 import static com.cavetale.mytems.util.Items.tooltip;
 import static net.kyori.adventure.text.Component.empty;
 import static net.kyori.adventure.text.Component.text;
@@ -34,34 +42,36 @@ import static net.kyori.adventure.text.format.TextColor.color;
 
 @Getter
 public final class Dividers implements Mytem {
+    private static final int MAX_RADIUS = 256;
+    private static final int COLOR_HEX = 0x95b8d1;
     private final Mytems key;
     private final Component displayName;
     private ItemStack prototype;
     private static final List<List<Vec2i>> BASE_CIRCLES = new ArrayList<>();
-    private static final int MAX_RADIUS = 256;
 
     public Dividers(final Mytems key) {
         this.key = key;
-        this.displayName = text("Dividers", color(0xE1C16E));
+        this.displayName = text("Dividers", color(COLOR_HEX));
     }
 
     @Override
     public void enable() {
         this.prototype = new ItemStack(key.material);
         prototype.editMeta(meta -> {
+                key.markItemMeta(meta);
                 tooltip(meta, List.of(displayName,
-                                      text(tiny("Plan out circles on the"), GRAY),
-                                      text(tiny("ground and build them"), GRAY),
-                                      text(tiny("accurately with ease"), GRAY),
+                                      text(tiny("Plan out circles and build"), GRAY),
+                                      text(tiny("them accurately with ease"), GRAY),
                                       empty(),
                                       textOfChildren(Mytems.MOUSE_LEFT, text(" Set Center", GRAY)),
-                                      textOfChildren(Mytems.MOUSE_RIGHT, text(" Draw Circle", GRAY))));
+                                      textOfChildren(Mytems.MOUSE_RIGHT, text(" Draw Circle", GRAY)),
+                                      textOfChildren(Mytems.MOUSE_CURSOR, Mytems.MOUSE_RIGHT, text(" Clear selection", GRAY))));
                 meta.setUnbreakable(true);
-                meta.addItemFlags(ItemFlag.values());
-                key.markItemMeta(meta);
+                Attr.addNumber(meta, Attribute.PLAYER_BLOCK_INTERACTION_RANGE, "dividers_range", 5.5, EquipmentSlotGroup.HAND);
+                meta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
             });
         BASE_CIRCLES.add(List.of(Vec2i.of(0, 0)));
-        for (int radius = 1; radius < MAX_RADIUS; radius += 1) {
+        for (int radius = 1; radius <= MAX_RADIUS; radius += 1) {
             BASE_CIRCLES.add(null);
         }
     }
@@ -75,41 +85,63 @@ public final class Dividers implements Mytem {
     public void onPlayerLeftClick(PlayerInteractEvent event, Player player, ItemStack item) {
         if (!event.hasBlock()) return;
         event.setCancelled(true);
-        click(player, event.getClickedBlock(), false);
+        click(player, event.getClickedBlock(), false, faceToAxis(event.getBlockFace()));
     }
 
     @Override
     public void onPlayerRightClick(PlayerInteractEvent event, Player player, ItemStack item) {
         if (!event.hasBlock()) return;
         event.setCancelled(true);
-        click(player, event.getClickedBlock(), true);
+        click(player, event.getClickedBlock(), true, faceToAxis(event.getBlockFace()));
     }
 
-    private void click(Player player, Block block, boolean right) {
-        DividersSession session = sessionOf(player).getFavorites().getOrSet(DividersSession.class, DividersSession::new);
-        if (session.drawing) return;
+    private static Axis faceToAxis(BlockFace face) {
+        return switch (face) {
+        case EAST -> Axis.X;
+        case WEST -> Axis.X;
+        case NORTH -> Axis.Z;
+        case SOUTH -> Axis.Z;
+        case UP -> Axis.Y;
+        case DOWN -> Axis.Y;
+        default -> Axis.Y;
+        };
+    }
+
+    private void click(Player player, Block block, boolean right, Axis axis) {
+        DividersSession session = Session.of(player).getFavorites().getOrSet(DividersSession.class, DividersSession::new);
         if (!PlayerBlockAbilityQuery.Action.BUILD.query(player, block)) {
             soundFail(player);
             return;
         }
         if (!block.getWorld().getName().equals(session.world)) {
+            session.clearBlocks();
             session.reset();
             session.world = block.getWorld().getName();
         }
         if (!right) {
             session.point1 = Vec3i.of(block);
-            draw(player, List.of(session.point1));
-            player.sendActionBar(text("Center " + session.point1, AQUA));
+            session.axis = axis;
+            draw(player, session, List.of(session.point1));
+            player.sendActionBar(textOfChildren(key,
+                                                text("Center ", GRAY), text("" + session.point1, AQUA),
+                                                text(" " + axis.name(), color(COLOR_HEX)), text(tiny("axis"), GRAY)));
         } else {
             if (session.point1 == null) {
                 session.point1 = Vec3i.of(block);
-                draw(player, List.of(session.point1));
+                session.axis = axis;
+                draw(player, session, List.of(session.point1));
             } else {
                 session.point2 = Vec3i.of(block);
             }
         }
         if (right && session.point1 != null && session.point2 != null) {
-            drawCircle(player, session, session.point1, session.point2);
+            drawCircle(player, session, session.point1, session.point2, session.axis);
+            player.sendMessage(textOfChildren(key,
+                                              text("(" + session.point1 + ")", color(COLOR_HEX)),
+                                              text(" " + session.axis.name(), color(COLOR_HEX)), text(tiny("axis"), GRAY),
+                                              text(" " + session.radius, color(COLOR_HEX)), text(tiny("radius"), GRAY),
+                                              text(" " + (session.blocks.size() - 1), color(COLOR_HEX)), text(tiny("blocks"), GRAY))
+                               .insertion("(" + session.point1 + ") (" + session.point2 + ")"));
             soundDraw(player);
         } else {
             soundUse(player);
@@ -148,7 +180,7 @@ public final class Dividers implements Mytem {
         return baseCircle;
     }
 
-    private boolean drawCircle(Player player, DividersSession session, Vec3i a, Vec3i b) {
+    private boolean drawCircle(Player player, DividersSession session, Vec3i a, Vec3i b, Axis axis) {
         World world = player.getWorld();
         Location center = a.toLocation(world).add(0.5, 0.5, 0.5);
         if (!center.isChunkLoaded()) {
@@ -166,69 +198,66 @@ public final class Dividers implements Mytem {
         }
         session.radius = radius;
         List<Vec2i> baseCircle = getBaseCircle(radius);
-        List<Vec3i> baseBlocks = new ArrayList<>();
+        final Vec3i r;
+        final Vec3i u;
+        switch (axis) {
+        case X:
+            r = Vec3i.of(0, 0, 1);
+            u = Vec3i.of(0, 1, 0);
+            break;
+        case Y:
+            r = Vec3i.of(1, 0, 0);
+            u = Vec3i.of(0, 0, 1);
+            break;
+        case Z:
+            r = Vec3i.of(1, 0, 0);
+            u = Vec3i.of(0, 1, 0);
+            break;
+        default:
+            throw new IllegalStateException("axis=" + axis);
+        }
+        final Set<Vec3i> blocks = new HashSet<>();
         for (Vec2i base : baseCircle) {
-            baseBlocks.add(Vec3i.of(a.x + base.x, a.y, a.z + base.z));
-            baseBlocks.add(Vec3i.of(a.x + base.x, a.y, a.z - base.z));
-            baseBlocks.add(Vec3i.of(a.x - base.x, a.y, a.z + base.z));
-            baseBlocks.add(Vec3i.of(a.x - base.x, a.y, a.z - base.z));
-            baseBlocks.add(Vec3i.of(a.x + base.z, a.y, a.z + base.x));
-            baseBlocks.add(Vec3i.of(a.x + base.z, a.y, a.z - base.x));
-            baseBlocks.add(Vec3i.of(a.x - base.z, a.y, a.z + base.x));
-            baseBlocks.add(Vec3i.of(a.x - base.z, a.y, a.z - base.x));
+            blocks.add(a.add(r.multiply(+base.x)).add(u.multiply(+base.z)));
+            blocks.add(a.add(r.multiply(+base.x)).add(u.multiply(-base.z)));
+            blocks.add(a.add(r.multiply(-base.x)).add(u.multiply(+base.z)));
+            blocks.add(a.add(r.multiply(-base.x)).add(u.multiply(-base.z)));
+            blocks.add(a.add(r.multiply(+base.z)).add(u.multiply(+base.x)));
+            blocks.add(a.add(r.multiply(+base.z)).add(u.multiply(-base.x)));
+            blocks.add(a.add(r.multiply(-base.z)).add(u.multiply(+base.x)));
+            blocks.add(a.add(r.multiply(-base.z)).add(u.multiply(-base.x)));
         }
-        List<Vec3i> blocks = new ArrayList<>();
         blocks.add(a);
-        LOOP:
-        for (Vec3i base : baseBlocks) {
-            if (!world.isChunkLoaded(base.x >> 4, base.z >> 4)) continue;
-            Block block = base.toBlock(world);
-            while (isFullBlock(block) && block.getY() < world.getMaxHeight()) {
-                Block above = block.getRelative(0, 1, 0);
-                if (!isFullBlock(above)) break;
-                block = above;
-            }
-            while (!isFullBlock(block)) {
-                if (block.getY() <= world.getMinHeight()) continue LOOP;
-                block = block.getRelative(0, -1, 0);
-            }
-            Vec3i vec = Vec3i.of(block);
-            if (blocks.contains(vec)) continue;
-            blocks.add(vec);
-        }
-        draw(player, blocks);
+        draw(player, session, List.copyOf(blocks));
         return true;
     }
 
-    private void draw(Player player, List<Vec3i> blocks) {
-        DividersSession session = sessionOf(player).getFavorites().getOrSet(DividersSession.class, DividersSession::new);
-        if (session.drawing) return;
-        session.drawing = true;
-        Bukkit.getScheduler().runTaskLater(MytemsPlugin.getInstance(), () -> {
-                session.drawing = false;
-                final World world = player.getWorld();
-                if (!player.isOnline() || !world.getName().equals(session.world)) {
-                    return;
-                }
-                if (session.blocks != null) {
-                    for (Vec3i vec : session.blocks) {
-                        if (!world.isChunkLoaded(vec.x >> 4, vec.z >> 4)) continue;
-                        Block block = vec.toBlock(world);
-                        player.sendBlockChange(block.getLocation(), block.getBlockData());
-                    }
-                    session.blocks = null;
-                }
-                BlockData fake = Material.BUDDING_AMETHYST.createBlockData();
-                for (Vec3i vec : blocks) {
-                    if (!world.isChunkLoaded(vec.x >> 4, vec.z >> 4)) continue;
-                    Block block = vec.toBlock(world);
-                    player.sendBlockChange(block.getLocation(), fake);
-                }
-                player.sendActionBar(textOfChildren(key, text(" Drawn circle at "),
-                                                    text(session.point1.x + " " + session.point1.z, GOLD),
-                                                    text(" radius "), text(session.radius, GOLD)));
-                session.blocks = blocks;
-            }, 2L);
+    private void draw(Player player, DividersSession session, List<Vec3i> blocks) {
+        final World world = player.getWorld();
+        session.clearBlocks();
+        session.blocks = new HashMap<>();
+        for (Vec3i vector : blocks) {
+            if (!world.isChunkLoaded(vector.x >> 4, vector.z >> 4)) {
+                continue;
+            }
+            final Cuboid cuboid = new Cuboid(vector.x, vector.y, vector.z,
+                                             vector.x, vector.y, vector.z);
+            final CuboidOutline outline = new CuboidOutline(world, cuboid);
+            outline.showOnlyTo(player);
+            outline.spawn();
+            outline.glow(Color.fromRGB(COLOR_HEX));
+            session.blocks.put(vector, outline);
+        }
+    }
+
+    @Override
+    public void onPlayerInventoryClick(InventoryClickEvent event, Player player, ItemStack item) {
+        if (!event.isRightClick()) return;
+        final DividersSession session = DividersSession.of(player);
+        session.clearBlocks();
+        session.reset();
+        soundUse(player);
+        event.setCancelled(true);
     }
 
     @Override
--- src/main/java/com/cavetale/mytems/item/dividers/DividersSession.java
+++ src/main/java/com/cavetale/mytems/item/dividers/DividersSession.java
@@ -1,20 +1,43 @@
 package com.cavetale.mytems.item.dividers;
 
 import com.cavetale.core.struct.Vec3i;
-import java.util.List;
+import com.cavetale.mytems.item.axis.CuboidOutline;
+import com.cavetale.mytems.session.Favorite;
+import com.cavetale.mytems.session.Session;
+import java.util.Map;
+import org.bukkit.Axis;
+import org.bukkit.entity.Player;
 
-public final class DividersSession {
-    protected boolean drawing;
+public final class DividersSession implements Favorite {
     protected String world;
     protected Vec3i point1;
     protected Vec3i point2;
+    protected Axis axis;
     protected int radius;
-    protected List<Vec3i> blocks;
+    protected Map<Vec3i, CuboidOutline> blocks;
+
+    @Override
+    public void onDisable() {
+        clearBlocks();
+        reset();
+    }
 
     protected void reset() {
         world = null;
         point1 = null;
         point2 = null;
         blocks = null;
+    }
+
+    protected void clearBlocks() {
+        if (blocks == null) return;
+        for (CuboidOutline outline : blocks.values()) {
+            outline.remove();
+        }
+        blocks.clear();
+    }
+
+    public static DividersSession of(Player player) {
+        return Session.of(player).getFavorites().getOrSet(DividersSession.class, DividersSession::new);
     }
 }
