636fceee00b83f7413fbdb266e8ab71bf029ca28
StarTux <ths.loy@gmail.com>
2025-08-11 16:46:23 +0200

    Implement the golf club

--- src/main/java/com/cavetale/mytems/item/golf/GolfClub.java
+++ src/main/java/com/cavetale/mytems/item/golf/GolfClub.java
@@ -1,17 +1,33 @@
 package com.cavetale.mytems.item.golf;
 
+import com.cavetale.core.event.block.PlayerBlockAbilityQuery;
+import com.cavetale.core.event.block.PlayerChangeBlockEvent;
 import com.cavetale.mytems.Mytem;
 import com.cavetale.mytems.Mytems;
+import com.cavetale.worldmarker.block.BlockMarker;
 import java.util.List;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import net.kyori.adventure.text.Component;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
+import org.bukkit.SoundCategory;
+import org.bukkit.block.Block;
+import org.bukkit.entity.FallingBlock;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.RayTraceResult;
+import org.bukkit.util.Vector;
+import static com.cavetale.core.font.Unicode.tiny;
 import static com.cavetale.mytems.util.Items.tooltip;
+import static com.cavetale.mytems.util.Text.roman;
+import static net.kyori.adventure.text.Component.text;
+import static net.kyori.adventure.text.Component.textOfChildren;
+import static net.kyori.adventure.text.format.NamedTextColor.*;
 
 @RequiredArgsConstructor @Getter
 public final class GolfClub implements Mytem {
@@ -29,7 +45,10 @@ public final class GolfClub implements Mytem {
     public void enable() {
         displayName = quality.getDisplayComponent();
         prototype = new ItemStack(key.material);
-        tooltip(prototype, List.of(displayName));
+        tooltip(prototype, List.of(displayName,
+                                   text(tiny("tier " + roman(quality.ordinal() + 1)), quality.getTextColor()),
+                                   textOfChildren(text(tiny("strength "), GRAY), text(quality.getIntegerStrength(), WHITE)),
+                                   textOfChildren(Mytems.MOUSE_RIGHT, text(" Swing", GRAY))));
         key.markItemStack(prototype);
     }
 
@@ -41,6 +60,39 @@ public final class GolfClub implements Mytem {
     @Override
     public void onPlayerRightClick(PlayerInteractEvent event, Player player, ItemStack item) {
         event.setUseItemInHand(Event.Result.DENY);
-        if (!event.hasBlock() || event.getClickedBlock().getType() != Material.DRAGON_EGG) return;
+        if (!event.hasBlock()) {
+            return;
+        }
+        final Block block = event.getClickedBlock();
+        if (block.getType() != Material.DRAGON_EGG) {
+            return;
+        }
+        event.setUseInteractedBlock(Event.Result.DENY);
+        if (BlockMarker.hasId(block)) {
+            return;
+        }
+        if (!PlayerBlockAbilityQuery.Action.BUILD.query(player, block)) {
+            return;
+        }
+        final RayTraceResult rayTrace = player.rayTraceBlocks(4.0);
+        if (rayTrace == null || rayTrace.getHitBlock() == null || !block.equals(rayTrace.getHitBlock())) {
+            return;
+        }
+        // No return!
+        new PlayerChangeBlockEvent(player, block, Material.AIR.createBlockData(), item).callEvent();
+        final Vector ballCenter = block.getLocation().add(0.5, 1.125, 0.5).toVector();
+        final Vector hitPoint = rayTrace.getHitPosition();
+        final Vector velocity = ballCenter.subtract(hitPoint)
+            .normalize()
+            .multiply(quality.getStrength());
+        block.setType(Material.AIR);
+        final Location ballLocation = block.getLocation().add(0.5, 0.0, 0.5);
+        final FallingBlock ball = block.getWorld().spawn(ballLocation, FallingBlock.class, e -> {
+                e.setBlockData(Material.DRAGON_EGG.createBlockData());
+                e.setVelocity(velocity);
+            });
+        ballLocation.getWorld().playSound(ballLocation, Sound.BLOCK_DISPENSER_LAUNCH, SoundCategory.MASTER, 1f, 0.5f);
+        ballLocation.getWorld().playSound(ballLocation, Sound.BLOCK_NOTE_BLOCK_IRON_XYLOPHONE, SoundCategory.MASTER, 1f, 0.5f);
+        ballLocation.getWorld().spawnParticle(Particle.BLOCK, ballLocation.clone().add(0, 0.5, 0), 32, 0.4, 0.125, 0.4, 0.0, Material.DRAGON_EGG.createBlockData());
     }
 }
--- src/main/java/com/cavetale/mytems/item/golf/GolfClubQuality.java
+++ src/main/java/com/cavetale/mytems/item/golf/GolfClubQuality.java
@@ -13,13 +13,14 @@ import static net.kyori.adventure.text.format.TextColor.color;
 @Getter
 @RequiredArgsConstructor
 public enum GolfClubQuality {
-    WOOD(Mytems.WOODEN_GOLF_CLUB, color(0xa47449)),
-    COPPER(Mytems.COPPER_GOLF_CLUB, color(0xb87333)),
-    IRON(Mytems.IRON_GOLF_CLUB, NamedTextColor.GRAY),
-    GOLD(Mytems.GOLDEN_GOLF_CLUB, NamedTextColor.GOLD),
-    DIAMOND(Mytems.DIAMOND_GOLF_CLUB, NamedTextColor.AQUA),
+    WOOD(0.5, Mytems.WOODEN_GOLF_CLUB, color(0xa47449)),
+    COPPER(1.0, Mytems.COPPER_GOLF_CLUB, color(0xb87333)),
+    IRON(1.5, Mytems.IRON_GOLF_CLUB, NamedTextColor.GRAY),
+    GOLD(2.0, Mytems.GOLDEN_GOLF_CLUB, NamedTextColor.GOLD),
+    DIAMOND(2.5, Mytems.DIAMOND_GOLF_CLUB, NamedTextColor.AQUA),
     ;
 
+    private final double strength;
     private final Mytems mytems;
     private final TextColor textColor;
 
@@ -29,6 +30,10 @@ public enum GolfClubQuality {
 
     public Component getDisplayComponent() {
         return text(getDisplayName(), textColor);
+    }
+
+    public int getIntegerStrength() {
+        return (int) Math.round(strength * 20.0);
     }
 
     public static GolfClubQuality of(Mytems mytems) {
