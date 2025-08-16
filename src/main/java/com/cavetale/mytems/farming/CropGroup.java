bad8bc280badc8046bace9a1937f24687f34b1e2
StarTux <ths.loy@gmail.com>
2025-08-16 15:10:01 +0200

    Move the GrowthType to the CropGroup

--- src/main/java/com/cavetale/mytems/farming/CropGroup.java
+++ src/main/java/com/cavetale/mytems/farming/CropGroup.java
@@ -13,76 +13,77 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum CropGroup {
     // Garden Beds
-    BEET(PlantLocation.GARDEN_BED, WateringNeed.THIRSTY),
-    CABBAGE(PlantLocation.GARDEN_BED, WateringNeed.THIRSTY),
-    CARROT(PlantLocation.GARDEN_BED, WateringNeed.THIRSTY),
-    GARLIC(PlantLocation.GARDEN_BED, WateringNeed.THIRSTY),
-    LEEK(PlantLocation.GARDEN_BED, WateringNeed.THIRSTY),
-    LETTUCE(PlantLocation.GARDEN_BED, WateringNeed.THIRSTY),
-    ONION(PlantLocation.GARDEN_BED, WateringNeed.THIRSTY),
-    PARSNIP(PlantLocation.GARDEN_BED, WateringNeed.THIRSTY),
-    PINEAPPLE(PlantLocation.GARDEN_BED, WateringNeed.MODERATE), // Grows as a terrestrial herbaceous plant
-    POTATO(PlantLocation.GARDEN_BED, WateringNeed.MODERATE),
-    RADISH(PlantLocation.GARDEN_BED, WateringNeed.THIRSTY),
+    BEET(PlantLocation.GARDEN_BED, GrowthType.ROOT, WateringNeed.THIRSTY),
+    CABBAGE(PlantLocation.GARDEN_BED, GrowthType.LEAFY, WateringNeed.THIRSTY),
+    CARROT(PlantLocation.GARDEN_BED, GrowthType.ROOT, WateringNeed.THIRSTY),
+    GARLIC(PlantLocation.GARDEN_BED, GrowthType.ROOT, WateringNeed.THIRSTY),
+    LEEK(PlantLocation.GARDEN_BED, GrowthType.ROOT, WateringNeed.THIRSTY),
+    LETTUCE(PlantLocation.GARDEN_BED, GrowthType.LEAFY, WateringNeed.THIRSTY),
+    ONION(PlantLocation.GARDEN_BED, GrowthType.ROOT, WateringNeed.THIRSTY),
+    PARSNIP(PlantLocation.GARDEN_BED, GrowthType.ROOT, WateringNeed.THIRSTY),
+    PINEAPPLE(PlantLocation.GARDEN_BED, GrowthType.HERBACEOUS, WateringNeed.MODERATE), // Grows as a terrestrial herbaceous plant
+    POTATO(PlantLocation.GARDEN_BED, GrowthType.ROOT, WateringNeed.MODERATE),
+    RADISH(PlantLocation.GARDEN_BED, GrowthType.ROOT, WateringNeed.THIRSTY),
 
     // Fields
-    CORN(PlantLocation.FIELD, WateringNeed.MODERATE),
-    OAT(PlantLocation.FIELD, WateringNeed.MODERATE),
-    WHEAT(PlantLocation.FIELD, WateringNeed.MODERATE),
-    PUMPKIN(PlantLocation.FIELD, WateringNeed.HARDY),
-    SQUASH(PlantLocation.FIELD, WateringNeed.MODERATE),
-    WATERMELON(PlantLocation.FIELD, WateringNeed.MODERATE),
-    MELON(PlantLocation.FIELD, WateringNeed.MODERATE),
-    HONEYDEW(PlantLocation.FIELD, WateringNeed.MODERATE),
-    RICE(PlantLocation.FIELD, WateringNeed.THIRSTY), // Rice needs flooded fields
+    CORN(PlantLocation.FIELD, GrowthType.HERBACEOUS, WateringNeed.MODERATE),
+    OAT(PlantLocation.FIELD, GrowthType.GRAIN, WateringNeed.MODERATE),
+    WHEAT(PlantLocation.FIELD, GrowthType.GRAIN, WateringNeed.MODERATE),
+    PUMPKIN(PlantLocation.FIELD, GrowthType.GOURD, WateringNeed.HARDY),
+    SQUASH(PlantLocation.FIELD, GrowthType.GOURD, WateringNeed.MODERATE),
+    WATERMELON(PlantLocation.FIELD, GrowthType.GOURD, WateringNeed.MODERATE),
+    MELON(PlantLocation.FIELD, GrowthType.GOURD, WateringNeed.MODERATE),
+    HONEYDEW(PlantLocation.FIELD, GrowthType.GOURD, WateringNeed.MODERATE),
+    RICE(PlantLocation.FIELD, GrowthType.GRAIN, WateringNeed.THIRSTY), // Rice needs flooded fields
 
     // Bushes
-    BERRY(PlantLocation.BUSH, WateringNeed.MODERATE),
-    BLACKBERRY(PlantLocation.BUSH, WateringNeed.MODERATE),
-    BLUEBERRY(PlantLocation.BUSH, WateringNeed.MODERATE),
-    COFFEE(PlantLocation.BUSH, WateringNeed.HARDY),
-    EGGPLANT(PlantLocation.BUSH, WateringNeed.MODERATE),
-    GOOSEBERRY(PlantLocation.BUSH, WateringNeed.MODERATE),
-    PEPPER(PlantLocation.BUSH, WateringNeed.MODERATE),
-    PLANTAIN(PlantLocation.BUSH, WateringNeed.MODERATE), // Large herbaceous plant, similar to banana
-    RASPBERRY(PlantLocation.BUSH, WateringNeed.MODERATE),
-    STRAWBERRY(PlantLocation.BUSH, WateringNeed.MODERATE),
-    TEA(PlantLocation.BUSH, WateringNeed.HARDY),
-    TOMATO(PlantLocation.BUSH, WateringNeed.MODERATE),
+    BERRY(PlantLocation.BUSH, GrowthType.BUSH, WateringNeed.MODERATE),
+    BLACKBERRY(PlantLocation.BUSH, GrowthType.BUSH, WateringNeed.MODERATE),
+    BLUEBERRY(PlantLocation.BUSH, GrowthType.BUSH, WateringNeed.MODERATE),
+    COFFEE(PlantLocation.BUSH, GrowthType.BUSH, WateringNeed.HARDY),
+    EGGPLANT(PlantLocation.BUSH, GrowthType.HERBACEOUS, WateringNeed.MODERATE),
+    GOOSEBERRY(PlantLocation.BUSH, GrowthType.BUSH, WateringNeed.MODERATE),
+    PEPPER(PlantLocation.BUSH, GrowthType.BUSH, WateringNeed.MODERATE),
+    PLANTAIN(PlantLocation.BUSH, GrowthType.TREE, WateringNeed.MODERATE), // Large herbaceous plant, similar to banana
+    RASPBERRY(PlantLocation.BUSH, GrowthType.BUSH, WateringNeed.MODERATE),
+    STRAWBERRY(PlantLocation.BUSH, GrowthType.HERBACEOUS, WateringNeed.MODERATE),
+    TEA(PlantLocation.BUSH, GrowthType.BUSH, WateringNeed.HARDY),
+    TOMATO(PlantLocation.BUSH, GrowthType.HERBACEOUS, WateringNeed.MODERATE),
 
     // Trellis
-    CUCUMBER(PlantLocation.TRELLIS, WateringNeed.THIRSTY),
-    GRAPE(PlantLocation.TRELLIS, WateringNeed.MODERATE),
-    KIWI(PlantLocation.TRELLIS, WateringNeed.MODERATE),
-    PITAYA(PlantLocation.TRELLIS, WateringNeed.MODERATE), // Climbing cactus vine
+    CUCUMBER(PlantLocation.TRELLIS, GrowthType.GOURD, WateringNeed.THIRSTY),
+    GRAPE(PlantLocation.TRELLIS, GrowthType.VINE, WateringNeed.MODERATE),
+    KIWI(PlantLocation.TRELLIS, GrowthType.VINE, WateringNeed.MODERATE),
+    PITAYA(PlantLocation.TRELLIS, GrowthType.CACTUS, WateringNeed.MODERATE), // Climbing cactus vine
 
     // Trees
-    APPLE(PlantLocation.TREE, WateringNeed.NONE),
-    AVOCADO(PlantLocation.TREE, WateringNeed.NONE),
-    BANANA(PlantLocation.TREE, WateringNeed.NONE),
-    CHERRY(PlantLocation.TREE, WateringNeed.NONE),
-    COCONUT(PlantLocation.TREE, WateringNeed.NONE),
-    FIG(PlantLocation.TREE, WateringNeed.NONE),
-    LEMON(PlantLocation.TREE, WateringNeed.NONE),
-    OLIVE(PlantLocation.TREE, WateringNeed.NONE),
-    ORANGE(PlantLocation.TREE, WateringNeed.NONE),
-    PLUM(PlantLocation.TREE, WateringNeed.NONE),
-    COCOA(PlantLocation.TREE, WateringNeed.NONE),
+    APPLE(PlantLocation.TREE, GrowthType.TREE, WateringNeed.NONE),
+    AVOCADO(PlantLocation.TREE, GrowthType.TREE, WateringNeed.NONE),
+    BANANA(PlantLocation.TREE, GrowthType.TREE, WateringNeed.NONE),
+    CHERRY(PlantLocation.TREE, GrowthType.TREE, WateringNeed.NONE),
+    COCONUT(PlantLocation.TREE, GrowthType.TREE, WateringNeed.NONE),
+    FIG(PlantLocation.TREE, GrowthType.TREE, WateringNeed.NONE),
+    LEMON(PlantLocation.TREE, GrowthType.TREE, WateringNeed.NONE),
+    OLIVE(PlantLocation.TREE, GrowthType.TREE, WateringNeed.NONE),
+    ORANGE(PlantLocation.TREE, GrowthType.TREE, WateringNeed.NONE),
+    PLUM(PlantLocation.TREE, GrowthType.TREE, WateringNeed.NONE),
+    COCOA(PlantLocation.TREE, GrowthType.COCOA, WateringNeed.NONE),
 
-    MUSHROOM(PlantLocation.OTHER, WateringNeed.NONE),
+    MUSHROOM(PlantLocation.OTHER, GrowthType.MUSHROOM, WateringNeed.NONE),
 
     // Aquatic
-    NORI(PlantLocation.AQUATIC, WateringNeed.NONE),
-    WAKAME(PlantLocation.AQUATIC, WateringNeed.NONE),
+    NORI(PlantLocation.AQUATIC, GrowthType.KELP, WateringNeed.NONE),
+    WAKAME(PlantLocation.AQUATIC, GrowthType.KELP, WateringNeed.NONE),
 
     // Fantasy
-    NETHER_WART(PlantLocation.OTHER, WateringNeed.NONE),
-    DRAGONBLOOM(PlantLocation.OTHER, WateringNeed.NONE),
-    GLOWROOT(PlantLocation.OTHER, WateringNeed.NONE),
-    CHORUS(PlantLocation.OTHER, WateringNeed.NONE),
-    GLOW_BERRY(PlantLocation.OTHER, WateringNeed.NONE),
+    NETHER_WART(PlantLocation.OTHER, GrowthType.NETHER_WART, WateringNeed.NONE),
+    DRAGONBLOOM(PlantLocation.OTHER, GrowthType.CACTUS, WateringNeed.NONE),
+    GLOWROOT(PlantLocation.OTHER, GrowthType.ROOT, WateringNeed.NONE),
+    CHORUS(PlantLocation.OTHER, GrowthType.CHORUS, WateringNeed.NONE),
+    GLOW_BERRY(PlantLocation.OTHER, GrowthType.VINE, WateringNeed.NONE),
     ;
 
     private final PlantLocation plantLocation;
+    private final GrowthType growthType;
     private final WateringNeed wateringNeed;
 }
